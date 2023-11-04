02cb5a9b7222267a1ff1d8e388170b39a744e1a3
Weslley da Silva <39630698+weesdasilva@users.noreply.github.com>
2023-11-04 10:51:53 -0300

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,7 +1,60 @@
+#Create VPC in us-east-1
+resource "aws_vpc" "vpc_master" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${terraform.workspace}-vpc"
+  }
+
+}
+
+#Get all available AZ's in VPC for master region
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "subnet" {
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "${terraform.workspace}-subnet"
+  }
+}
+
+#Create SG for allowing TCP/22 from anywhere, THIS IS FOR TESTING ONLY
+resource "aws_security_group" "sg" {
+  name        = "${terraform.workspace}-sg"
+  description = "Allow TCP/22"
+  vpc_id      = aws_vpc.vpc_master.id
+  ingress {
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${terraform.workspace}-securitygroup"
+  }
+}
+
+
+data "aws_ssm_parameter" "linuxAmi" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0df435f331839b2d6"
+  ami           = data.aws_ssm_parameter.linuxAmi.value
   instance_type = "t3.micro"
-  subnet_id     = "subnet-0d4205df3c8e38d79"
+  subnet_id     = aws_subnet.subnet.id
   tags = {
     Name = var.instance_name
   }
